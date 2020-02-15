2e55667a00e228f6b611f187e0cead742a2d3caa
Sarah <sarkwon89@gmail.com>
2020-02-15 14:29:28 -0800

    finished about page need styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navbar from '../src/components/Navbar'
 import Home from '../src/components/pages/Home'
+import About from '../src/components/pages/About'
+
+
 import './App.css'
 
 function App() {
   return (
-    <div className="test">
-      <Navbar />
-      <Home />
-    </div>
+      <Router>
+        <div className="test">
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </Router>
   );
 }
 
