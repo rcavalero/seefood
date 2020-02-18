c47e0711667b4f25cb86133ab955ad8e8e210c8c
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-02-17 21:34:53 -0800

    revise update functionality

--- src/components/UpdateCard.js
+++ src/components/UpdateCard.js
@@ -8,10 +8,12 @@ import M from "materialize-css";
 class Update extends Component {
 
     state = {
+        // search: "",
         ingredients: [],
-        selectedIngredient: [],
+        // selectedIngredient: [],
         id: 0,
         ingredient: "",
+        // updIngredient:{        
         asin1: "",
         asin2: "",
         asin3: "",
@@ -34,19 +36,37 @@ class Update extends Component {
 
     handleIngredientSelection = event => {
         // Getting the value and name of the input which triggered the change
-        // const value = event.target.value;
+        const value = event.target.value;
         // const name = event.target.name;
         const key = event.target.id;
-        console.log(key);
+        console.log(event.target.value)
+        this.setState({ingredient: value})
         
+        const temp = this.state.ingredients;
+        let tempId = 0;
+
+        temp.forEach((item)=>{
+            if(item.ingredient === event.target.value){
+                tempId = item.id;
+                this.setState({id: tempId});
+            }
+            // console.log(item);
+        })
+
         // this.setState({id: key});
-        this.setState({id: 5});
+        
         // this.setState({
         //   [name]: value
         // });
 
-        API.getOneIngredient(this.state.id)
-        .then(res => this.setState({ selectedIngredient: res.data }))
+        API.getOneIngredient(tempId)
+        .then(res => this.setState({ 
+
+            asin1: res.data.asin1,
+            asin2: res.data.asin2,
+            asin3: res.data.asin3,
+            asin4: res.data.asin4
+         }))
 
         // Updating the input's state
       };
@@ -66,8 +86,13 @@ class Update extends Component {
       handleFormSubmit = event => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
         event.preventDefault();
-        API.updateIngredient(this.state);
+        API.updateIngredient(this.state.id, this.state);
+        console.log("state");
+        
+        console.log(this.state);
+        
         this.setState({  
+          id: 0,
           ingredient: "",
           asin1: "",
           asin2: "",
@@ -85,16 +110,16 @@ class Update extends Component {
                     <div className="input-field col s12">
                     <label>Select Ingredient</label>
                     <input
-                    value={this.state.ingredients.ingredient}
+                    value={this.state.ingredient}
                     onChange={this.handleIngredientSelection}
-                            name="ingredient"
+                            name={this.state.ingredients.ingredient}
                             list="ingredients"
                             type="text"
-                            id="ingredient"
+                            id={this.state.ingredients.id}
                     />
                         <datalist id="ingredients">
                           {this.state.ingredients.map(ingredient => (
-                            <option value={ingredient.ingredient} key={ingredient.id} />
+                            <option value={ingredient.ingredient} data-id={ingredient.id} key={ingredient.id} />
                           ))}
                         </datalist>
 
@@ -103,38 +128,39 @@ class Update extends Component {
 
                     <div className="row">
                         <div className="input-field col s12" >
-                            <input id="ASIN1" type="text"
-                                value={this.selectedIngredient.asin1}
+                        <label htmlFor="ASIN1up">ASIN 1</label>
+                            <input id="ASIN1up" type="text"
+                                value={this.state.asin1}
                                 name="asin1"
                                 onChange={this.handleInputChange} />
-                            <label htmlFor="ASIN1">ASIN 1</label>
+                            {/* <label htmlFor="ASIN1">ASIN 1</label> */}
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="ASIN2" type="text"
+                            <input id="ASIN2up" type="text"
                                 value={this.state.asin2}
                                 name="asin2"
                                 onChange={this.handleInputChange} />
-                            <label htmlFor="ASIN2">ASIN 2</label>
+                            <label htmlFor="ASIN2up">ASIN 2</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="ASIN3" type="text"
+                            <input id="ASIN3up" type="text"
                                 value={this.state.asin3}
                                 name="asin3"
                                 onChange={this.handleInputChange} />
-                            <label htmlFor="ASIN3">ASIN 3</label>
+                            <label htmlFor="ASIN3up">ASIN 3</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="ASIN4" type="text"
+                            <input id="ASIN4up" type="text"
                                 value={this.state.asin4}
                                 name="asin4"
                                 onChange={this.handleInputChange} />
-                            <label htmlFor="ASIN4">ASIN 4</label>
+                            <label htmlFor="ASIN4up">ASIN 4</label>
                         </div>
                     <div className="row">
                         <button className="btn btn-primary" onClick={this.handleFormSubmit}>
@@ -147,11 +173,5 @@ class Update extends Component {
     }
 }
 
-
-                    </div>
-                </div>
-                )
-            }
-        }
         
 export default Update;
