7cb3e95eb25828502ba44b1948ff3451db281056
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-02-18 13:17:34 -0800

    begin delete card functionality.

--- src/components/DeleteCard.js
+++ src/components/DeleteCard.js
@@ -2,11 +2,16 @@ import React from "react";
 import './styles/Delete.css';
 
 function DeleteCard(props) {
+    function clickHandler() {
+        props.deleteClickHandler(props.ingredients.id)
+    }
+
+
     return (
         <ul className="list-group">
             <li className="list-group-item">
-                <h3>Ingredient</h3>
-                <i class="far fa-trash-alt fa-3x"></i>
+                <h3>{props.ingredients.ingredient}</h3>
+                <button onClick={clickHandler}><i className="far fa-trash-alt fa-3x"></i></button>
             </li>
         </ul>
     )
