ac6f37c8b8880bc17cfed8e0dc5521055303f3e0
Sarah <sarkwon89@gmail.com>
2020-02-18 16:19:16 -0800

    got ingredients to load

--- src/components/AddRec.js
+++ src/components/AddRec.js
@@ -7,50 +7,79 @@ import M from "materialize-css";
 
 class AddRec extends Component {
 
-    componentDidMount() {
-        // Auto initialize all the things!
-        M.AutoInit();
+    state = {
+        ingredients: [],
+        name: "",
+        id: ""
     }
+
+    componentDidMount() {
+        API.getIngredients()
+            .then(res => this.setState({ ingredients: res.data }))
+            .then(M.AutoInit())
+            .catch(err => console.log(err));
+    }
+
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        const value = event.target.value;
+        const name = event.target.name;
+        
+        // Updating the input's state
+        this.setState({
+          [name]: value
+        });
+      };
+
 
     render() {
         return (
             <div className="container">
-                <h2>Add Brand Recommendation</h2>
-                <div className="row">
-                    <div className="input-field col s12">
-                        <select>
-                            <option value="" disabled selected>Choose your option</option>
-                            <option value="1">Option 1</option>
-                            <option value="2">Option 2</option>
-                            <option value="3">Option 3</option>
-                        </select>
-                        <label>Materialize Select</label>
-                    </div>
-                    <div className="input-field col s12" >
-                        <input id="BrandName1" type="text" />
-                        <label htmlFor="BrandName1">Brand Name 1</label>
-                    </div>
-                    <div className="input-field col s12" >
-                        <input id="Img1" type="url" />
-                        <label htmlFor="Img1">Image</label>
-                    </div>
-                    <div className="input-field col s12" >
-                        <input id="Price1" type="number" />
-                        <label htmlFor="Prife1">Price</label>
-                    </div>
-                    <div className="input-field col s12" >
-                        <input id="Url1" type="url" />
-                        <label htmlFor="Url1">Link to Amazon Product</label>
-                    </div>
-                    <div className="input-field col s12" >
-                        <button className="btn btn-primary" >
-                            Add Recommendation
-                    </button>
-                    </div>
-                </div>
+        <h2>Add Brand Recommendation</h2>
+        <div className="row">
+            <div className="input-field col s12">
+                {/* <select> */}
+                    <input
+                        value={this.state.ingredients.name}
+                        onChange={this.handleInputChange}
+                        name={this.state.ingredients.name}
+                        list="ingredients"
+                        type="text"
+                        id={this.state.ingredients.id}
+                    />
+                    <datalist id="ingredients">
+                        {this.state.ingredients.map(ing => (
+                            <option value={ing.name} id={ing.id} data-id={ing.id} key={ing.id} />
+                        ))}
+                    </datalist>
+                {/* </select> */}
+                <label>Select an ingredient</label>
             </div>
-        )
-    }
+            <div className="input-field col s12" >
+                <input id="BrandName1" type="text" />
+                <label htmlFor="BrandName1">Brand Name 1</label>
+            </div>
+            <div className="input-field col s12" >
+                <input id="Img1" type="url" />
+                <label htmlFor="Img1">Image</label>
+            </div>
+            <div className="input-field col s12" >
+                <input id="Price1" type="number" />
+                <label htmlFor="Prife1">Price</label>
+            </div>
+            <div className="input-field col s12" >
+                <input id="Url1" type="url" />
+                <label htmlFor="Url1">Link to Amazon Product</label>
+            </div>
+            <div className="input-field col s12" >
+                <button className="btn btn-primary" >
+                    Add Recommendation
+                    </button>
+            </div>
+        </div>
+    </div>
+    )
+}
 }
 
 export default AddRec
