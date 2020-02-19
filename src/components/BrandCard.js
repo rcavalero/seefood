9c435d14ce10b334c313bfffae762715e6e7cfca
Sarah <sarkwon89@gmail.com>
2020-02-19 12:27:30 -0800

    add cards

--- src/components/BrandCard.js
+++ src/components/BrandCard.js
@@ -1,35 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './styles/brandrec.css'
 
-class BrandCard extends Component {
 
-  componentDidMount () {
-    const script = document.createElement("script");
-
-    script.src = "//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US";
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    // const script = document.createElement("script");
-
-    // script.src = "";
-    // script.async = true;
-
-    // document.body.appendChild(script);
-}
-
-  amazonRecsScript = () => {
-    return '<script type="text/javascript">  amzn_assoc_placement = "adunit0"; amzn_assoc_search_bar = "true"; amzn_assoc_tracking_id = "sarkwon89-20"; amzn_assoc_ad_mode = "manual"; amzn_assoc_ad_type = "smart"; amzn_assoc_marketplace = "amazon"; amzn_assoc_region = "US"; amzn_assoc_title = "My Amazon Picks"; amzn_assoc_linkid = "c43adb62d8f81013cbdde7481af6d3ee"; amzn_assoc_asins = "B076TB6GW2,B009GFW8G4,B074H6X64W,B01J3TGH1E"; </script> <script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>'
-  }
-
-  render() {
-    return (
-      <div className="advertisement">
-      <div dangerouslySetInnerHTML={{ __html: this.amazonRecsScript() }}></div>
+function BrandCard() {
+  return (
+    <div className="container recontainer">
+      <div className="row recrow">
+        <div className="col s12 m4">
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://res.cloudinary.com/diad1aa5z/image/upload/v1582143170/Image_from_iOS_ctvefm.jpg" />
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+              <p><a href="#">This is a link</a></p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://res.cloudinary.com/diad1aa5z/image/upload/v1582143170/Image_from_iOS_ctvefm.jpg" />
+            </div>
+            <div className="card-content">
+              <h3>BRAND NAME</h3>
+              <hr />
+              <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://res.cloudinary.com/diad1aa5z/image/upload/v1582143170/Image_from_iOS_ctvefm.jpg" />
+            </div>
+            <div className="card-content">
+              <h3>BRAND NAME</h3>
+              <hr />
+              <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://res.cloudinary.com/diad1aa5z/image/upload/v1582143170/Image_from_iOS_ctvefm.jpg" />
+            </div>
+            <div className="card-content">
+              <h3>BRAND NAME</h3>
+              <hr />
+              <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
 
 export default BrandCard;
