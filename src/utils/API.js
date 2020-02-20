f2dafc155e66a66a758870fb214b26b669147dc9
Sarah <sarkwon89@gmail.com>
2020-02-20 15:24:56 -0800

    cleaned up folder enabled correct url

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const URL = "http://localhost:8080"
-// const URL = "https://seefoodar-api.herokuapp.com/"
+// const URL = "http://localhost:8080"
+const URL = "https://seefoodar-api.herokuapp.com/"
 
 const API = {
     getIngredients: ()=>{
